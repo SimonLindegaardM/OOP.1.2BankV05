9f5885a35e055b48a12cb3372a3a6319c715f34a
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-03-30 13:23:12 +0200

    Initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,43 @@
+﻿namespace BankV05
+{
+    /// <summary>
+    /// This class represents a very simple bank account.
+    /// Only the amount of money on the account is represented.
+    /// </summary>
+    public class BankAccount
+    {
+        private double _balance;
+        private string _Name;
+        private double _age;
+
+        public BankAccount(string Name)
+        {
+            _balance = 0.0;
+            _Name = Name;
+            _age = 23;
+        }
+
+        public double Balance
+        {
+            get { return _balance;}
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance = _balance + amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            _balance = _balance - amount;
+        }
+        public string Name
+        {
+            get { return _Name;}
+        } 
+        public double Age
+        {
+            get { return _age;}
+        }
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BankV05
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            BankAccount myAccount = new BankAccount("Simon Mathiasen");
+            BankAccount myAccount2= new BankAccount("Bo");
+            BankAccount myAccount3= new BankAccount("Per");
+            BankAccount myAccount4= new BankAccount("Lars");
+
+            System.Console.WriteLine($"{myAccount.Name} account is {myAccount.Age} years old");
+            myAccount.Deposit(2000);
+            myAccount2.Deposit(15000);
+            myAccount3.Deposit(5000);
+            myAccount4.Deposit(45000);
+            myAccount.Deposit(565000);
+            Console.WriteLine($"The balance on {myAccount.Name} is : {myAccount.Balance}");
+            Console.WriteLine($"The balance on {myAccount2.Name} is : {myAccount2.Balance}");
+            Console.WriteLine($"The balance on {myAccount3.Name} is : {myAccount3.Balance}");
+            Console.WriteLine($"The balance on {myAccount4.Name} is : {myAccount4.Balance}");
+
+            myAccount.Withdraw(1500);
+            Console.WriteLine($"The balance is : {myAccount.Balance}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
